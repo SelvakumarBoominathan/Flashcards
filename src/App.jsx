842e963a07bc67e34d1b7b5d80cf1e5b7a5d0a1c
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-26 00:47:44 +0530

    state hook added to eneble dynamic operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, React } from 'react';
+import './App.css';
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const step = 1;
+
+  const [step, setStep] = useState(1);
+
+  function handlePrevious(){
+
+    if (step>1){
+      setStep(step - 1)
+    }
+  }
+
+  function handleNext(){
+
+    if (step<3){
+      setStep(step + 1);
+    }
+    
+  }
+
+
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className='steps'>
+      <div className='numbers'>
+          <div className={step >=1 ? 'active': ""}>1</div>
+          <div className={step >=2 ? 'active': ""}>2</div>
+          <div className={step >=3 ? 'active': ""}>3</div>
+      </div>
 
-export default App
+        <p className="message">Step {step}: {messages[step-1]}</p>
+
+      <div className='buttons'>
+        <button onClick={handlePrevious} style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
+        <button onClick={handleNext} style={{backgroundColor: '#7950f2', color : '#fff'}}>Next</button>
+      </div>
+
+    </div>
+
+  )}
+
+export default App;
